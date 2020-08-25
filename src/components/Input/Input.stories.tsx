0e5fcfec1f5da5b9a045badb950c5731064a4fed
Robson Junior <jose.robson@splab.ufcg.edu.br>
2020-08-25 15:47:19 -0300

    feat: set up sizes for input

--- src/components/Input/Input.stories.tsx
+++ src/components/Input/Input.stories.tsx
@@ -6,29 +6,19 @@ import Input, { InputProps } from './index'
 export default {
   title: 'Work in progress/Input',
   component: Input,
+  argTypes: {
+    size: {
+      control: {
+        type: 'select',
+        options: ['regular', 'large'],
+      },
+    }
+  }
 } as Meta
 
 const Template: Story<InputProps> = (args) => <Input {...args} />
 
 export const Playground = Template.bind({})
 Playground.args = {
-  children: 'Input',
+  size: 'regular',
 }
-
-// import React from 'react'
-// import { withA11y } from '@storybook/addon-a11y'
-
-// import BrandTheme from '../Theme'
-// import { Input } from './index'
-
-// export default {
-//   title: 'Input',
-//   decorators: [withA11y],
-//   component: Input,
-// }
-
-// export function Playground() {
-//   return (
-//     <Input />
-//   )
-// }
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,13 +1,36 @@
-import React from 'react'
+import React, { PropsWithChildren } from 'react'
 import { Input as ThemeUIInput } from 'theme-ui'
 import { useInput } from 'reakit/Input'
 
-const BrandInput = (props: any) => {
-  const { ...restProps } = props
-  const { children, ...inputProps } = useInput({}, restProps)
-  return <ThemeUIInput {...inputProps} />
+const pickPadding = (size: Size) => {
+  const variants = {
+    regular: {
+      px: 3,
+      py: 2,
+    },
+    large: {
+      px: 3,
+      py: 3,
+    }
+  }
+
+  return variants?.[size] ?? variants.regular
 }
 
-export interface InputProps {}
+const BrandInput = (props: PropsWithChildren<InputProps>) => {
+  const { size = 'regular', ...restProps } = props
+  const { children, ...inputProps } = useInput({}, restProps)
+  return <ThemeUIInput sx={{...pickPadding(size)}} {...inputProps} />
+}
+
+export type Type = 'text' | 'password'
+export type Size =  'regular' | 'large'
+
+export interface InputProps {
+  /** Size of the input
+   * @default regular
+   * */
+  size?: Size
+}
 
 export default BrandInput
